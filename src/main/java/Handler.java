6085f9f4499134cf75d15b1693a327735c9f0784
David <david.rodriguez-a@mail.escuelaing.edu.co>
2019-03-14 11:36:26 -0500

    AWS implementacion

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,20 +1,76 @@
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.Writer;
+import java.security.spec.ECField;
 
-public class Handler implements RequestHandler<String, String> {
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
-    public String handleRequest(String s, Context context) {
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+
+
+public class Handler implements RequestStreamHandler {
+    JSONParser parser = new JSONParser();
+
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("Loading Java Lambda handler of ProxyWithStream");
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONObject responseJson = new JSONObject();
+        String s = null;
+        String responseCode = "200";
+
         try {
-            if(s.equals("list")) {
-                return Exchange.getList();
-            }else{
-                return Exchange.getExchange();
+            JSONObject event = (JSONObject)parser.parse(reader);
+            if (event.get("queryStringParameters") != null) {
+                JSONObject qps = (JSONObject)event.get("queryStringParameters");
+                if ( qps.get("s") != null) {
+                   s = (String)qps.get("s");
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            String greeting =null;
+            if(s.equals("list")){
+                greeting = Exchange.getList();
+            }else{
+                greeting = Exchange.getExchange();
+            }
+
+
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("input", event.toJSONString());
+            responseBody.put("message", greeting);
+
+            JSONObject headerJson = new JSONObject();
+            headerJson.put("x-custom-header", "my custom header value");
+
+            responseJson.put("isBase64Encoded", false);
+            responseJson.put("statusCode", responseCode);
+            responseJson.put("headers", headerJson);
+            responseJson.put("body", responseBody.toString());
+
+        } catch(ParseException pex) {
+            responseJson.put("statusCode", "400");
+            responseJson.put("exception", pex);
         }
-        return "Error";
+
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(responseJson.toJSONString());
+        writer.close();
     }
 }
